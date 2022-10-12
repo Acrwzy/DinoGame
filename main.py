5d7731a74da9213a7dc6d03d567c0c70484a6cbc
Aarav Kaushik <67853273+Acrwzy@users.noreply.github.com>
2022-10-13 01:08:43 +0530

    PyGame initialized; Window made

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 
+# Libraries used 
+import sys
+import pygame
+
+# Dimensions of the window
+WIDTH = 623
+HEIGHT = 150
+
+# Initializing PyGame Library
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Dino')
+
+def main():
+    # Main loop 
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit
+                sys.exit()
+        pygame.display.update
+
+main()
